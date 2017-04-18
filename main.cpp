6f93a2ad109485b83f675b941d4accf5883969c7
Matias Marceca <matias.marceca09@gmail.com>
2017-04-18 00:19:08 -0300

    binary tree - 50% of Vertical Balance and 30% of Remove Method - working progress

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@ int main(int argsc, char** args)
 {
     std::unique_ptr<BinaryTree> binaryTree = std::unique_ptr<BinaryTree>(new BinaryTree());
 
+    // For the moment the half of the
+    // balance algorithm is working
+    // with natural numbers in the tree.
+    // I have to test it with negatives numbers
+    // for to if this BLOW UP THROUGH THE AIR
     binaryTree->Insert(10);
     binaryTree->Insert(5);
     binaryTree->Insert(15);
     binaryTree->Insert(13);
+    binaryTree->Insert(19);
+    binaryTree->Insert(22);
+    binaryTree->Insert(23);
     binaryTree->Insert(20);
     binaryTree->Insert(11);
     binaryTree->Insert(14);

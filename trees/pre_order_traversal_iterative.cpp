22fb4d245b61d231620dc2c4fd2b79f2fe6bebf5
Matias M. Marceca <matias.marceca09@gmail.com>
2019-04-30 00:45:43 -0300

    Ready pre order traversal iterative. Yeah Bitch.  On branch leet_code_exercises  Changes to be committed: 	modified:   pre_order_traversal_iterative.cpp

--- trees/pre_order_traversal_iterative.cpp
+++ trees/pre_order_traversal_iterative.cpp
@@ -41,69 +41,65 @@ void ReleaseValues(Tree *head) {
   }
 }
 
-std::vector<int> PreOrderTraversalIter(Tree *head)
-{
+std::vector<int> PreOrderTraversalIter(Tree *head) {
   if (head == nullptr) return std::vector<int>({});
 
   // Safe Guard
   int i = -1;
 
-  Tree *tmp_node = head;
+  Tree *tmp_node = nullptr;
   std::vector<int> res = {};
-  std::stack<Tree *> tree_ptrs;
-  size_t size_tree_ptrs = 0;
+  std::stack<Tree *> stack_tree_ptrs;
 
-  tree_ptrs.push(tmp_node);
+  stack_tree_ptrs.push(head);
 
-  for (;;) {
-    if (size_tree_ptrs < tree_ptrs.size()) {
-      if (tmp_node->lower != nullptr) {
-        tree_ptrs.push(tmp_node->lower);
-        ++size_tree_ptrs;
+  while (i < 500) {
+    tmp_node = stack_tree_ptrs.top();
 
-        tmp_node = tmp_node->lower;
+    if (tmp_node->lower != nullptr) {
+      stack_tree_ptrs.push(tmp_node->lower);
+
+      ++i;
+      continue;
+    } else {
+      if (tmp_node->greater != nullptr) {
+        stack_tree_ptrs.push(tmp_node->greater);
+
+        ++i;
         continue;
       } else {
-        if (tmp_node->greater != nullptr) {
-          tree_ptrs.push(tmp_node->greater);
-          ++size_tree_ptrs;
+        // Adds a leaf to the result
+        // Array to be showed
+        res.push_back(tmp_node->n);
 
-          tmp_node = tmp_node->greater;
-          continue;
-        } else {
+        // Removing node from the Stack
+        stack_tree_ptrs.pop();
+
+        if (!stack_tree_ptrs.empty()) {
+          tmp_node = stack_tree_ptrs.top();
+
           res.push_back(tmp_node->n);
-          tmp_node = tree_ptrs.top();
-          tree_ptrs.pop();
+
+          stack_tree_ptrs.pop();
+
+          if (tmp_node->greater != nullptr) {
+            stack_tree_ptrs.push(tmp_node->greater);
+
+            continue;
+            ++i;
+          }
         }
       }
-    } else {
-
-      // TODO: This is not quite Right, becouse
-      // you are not saving the root node
-      // when you previusly save the lower child node.
-      //
-      // Think about that. How to Save the
-      // right node.
-      if (tmp_node->greater != nullptr) {
-        tree_ptrs.push(tmp_node->greater);
-
-        tmp_node = tmp_node->greater;
-        continue;
-      } else {
-        res.push_back(tmp_node->n);
-        tmp_node = tree_ptrs.top();
-        tree_ptrs.pop();
-      }
     }
 
-    if (tree_ptrs.size() == 0) break;
+   if (stack_tree_ptrs.empty()) break;
 
-    // Safe Guard to prevent Inf Loop
-    if ((++i) > 500) {
-      std::cout << "I go out thanks to \"i\" local variable . . . \n";
-      break;
-    }
+    ++i;
   }
+
+  // Safe Guard to prevent Inf Loop
+  if (i == 500)
+    std::cout << "I go out thanks to \"i\" local variable . . . \n";
 
   return res;
 }
@@ -111,10 +107,20 @@ std::vector<int> PreOrderTraversalIter(Tree *head)
 int main() {
   Tree *tree = new Tree(100);
 
-  std::forward_list<int> values = {100, 50, 25, 75, 60, 80, 20, 30, 150, 125, 175, 160, 180, 120, 130};
+  std::forward_list<int> values = {
+    // 100,
+    50, 150,
+    40, 60, 140, 160,
+    30, 45,55,70,130, 145, 155,170
+  };
 
   for (auto &e : values)
     Insert(tree, e);
+
+  std::vector<int> tree_values = PreOrderTraversalIter(tree);
+
+  for (auto &e : tree_values)
+    std::cout << e << ", ";
 
   ReleaseValues(tree);
   return 0;

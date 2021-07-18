8eec00cefa4fd48697ad2e8f43fd162415d67975
Matias M. Marceca <matias.marceca09@gmail.com>
2021-07-18 09:46:01 -0300

    add templated unit test file for test structures

--- structures/lists/single/main.cpp
+++ structures/lists/single/main.cpp
@@ -1,5 +1,6 @@
 #include "list.h"
 #include "circular_list.h"
+#include "unit_test.h"
 
 int main(int argc, char* args[])
 {
@@ -17,21 +18,21 @@ int main(int argc, char* args[])
       std::cout << "-- LengthEqualToOneAfterOnePushBack \t FAIL" << '\n';
       return -1;
   }
-  //
-  // if ( not LengthBackToCeroAfterOnePushAndOnePop( CircularSingleList<int>() )) {
-  //     std::cout << "-- LengthBackToCeroAfterOnePushAndOnePop \t FAIL" << '\n';
-  //     return -1;
-  // }
-  //
-  // if ( not LengthAfterThreePush( CircularSingleList<int>() )) {
-  //     std::cout << "-- LengthAfterThreePush \t FAIL" << '\n';
-  //     return -1;
-  // }
-  //
-  // if ( not ErrorAfterOnePushAndOnePop( CircularSingleList<int>() )) {
-  //     std::cout << "-- ErrorAfterOnePushAndOnePop \t FAIL" << '\n';
-  //     return -1;
-  // }
+
+  if ( not LengthBackToCeroAfterOnePushAndOnePop( CircularSingleList<int>() )) {
+      std::cout << "-- LengthBackToCeroAfterOnePushAndOnePop \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not LengthAfterThreePush( CircularSingleList<int>() )) {
+      std::cout << "-- LengthAfterThreePush \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not ErrorAfterOnePushAndOnePop( CircularSingleList<int>() )) {
+      std::cout << "-- ErrorAfterOnePushAndOnePop \t FAIL" << '\n';
+      return -1;
+  }
 
   std::cout << "\t ALL TEST SUCCEDED "<< '\n';
   return 0;
---
+++ structures/lists/single/unit_test.h
@@ -0,0 +1,48 @@
+// Unit Test
+template <typename T>
+bool LengthCeroAtEmptyConstruction(T && c)
+{
+  return c.length() == 0;
+}
+
+template <typename T>
+bool LengthGreaterThanCeroAtConstructionWithInitializerList(T&& c)
+{
+  return c.length() > 0;
+}
+
+template <typename T>
+bool LengthEqualToOneAfterOnePushBack(T&& c)
+{
+  c.Insert(1);
+  return c.length() == 1;
+}
+
+template <typename T>
+bool LengthBackToCeroAfterOnePushAndOnePop(T&& c)
+{
+  c.Insert(1);
+  c.Erase();
+  return c.length() == 0;
+}
+
+template <typename T>
+bool LengthAfterThreePush(T&& c)
+{
+  c.Insert(1);
+  c.Insert(2);
+  c.Insert(3);
+
+  return c.length() == 3;
+}
+
+template <typename T>
+bool ErrorAfterOnePushAndOnePop(T&& c)
+{
+  c.Insert(1);
+  c.Erase();
+  c.Erase();
+
+  return c.length() == false;
+}
+

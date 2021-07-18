706ef302f432921858e860ade5580f76a7f4189f
Matias M. Marceca <matias.marceca09@gmail.com>
2021-07-18 10:11:48 -0300

    add test from simple and singular link list

--- structures/lists/single/main.cpp
+++ structures/lists/single/main.cpp
@@ -4,36 +4,71 @@
 
 int main(int argc, char* args[])
 {
+  std::cout << "--- Single List Test" << '\n';
+  if ( not LengthCeroAtEmptyConstruction( SingleList<int>() ) ) {
+      std::cout << "\t-- LengthCeroAtEmptyConstruction \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not LengthGreaterThanCeroAtConstructionWithInitializerList( SingleList<int>({1, 2}) ) ) {
+      std::cout << "\t-- LengthGreaterThanCeroAtConstructionWithInitializerList \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not LengthEqualToOneAfterOnePushBack( SingleList<int>() )) {
+      std::cout << "\t-- LengthEqualToOneAfterOnePushBack \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not LengthBackToCeroAfterOnePushAndOnePop( SingleList<int>() )) {
+      std::cout << "\t-- LengthBackToCeroAfterOnePushAndOnePop \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not LengthAfterThreePush( SingleList<int>() )) {
+      std::cout << "\t-- LengthAfterThreePush \t FAIL" << '\n';
+      return -1;
+  }
+
+  if ( not ErrorAfterOnePushAndOnePop( SingleList<int>() )) {
+      std::cout << "\t-- ErrorAfterOnePushAndOnePop \t FAIL" << '\n';
+      return -1;
+  }
+
+  std::cout << "--- Circular Single List Test" << '\n';
   if ( not LengthCeroAtEmptyConstruction( CircularSingleList<int>() ) ) {
-      std::cout << "-- LengthCeroAtEmptyConstruction \t FAIL" << '\n';
+      std::cout << "\t-- LengthCeroAtEmptyConstruction \t FAIL" << '\n';
       return -1;
   }
 
   if ( not LengthGreaterThanCeroAtConstructionWithInitializerList( CircularSingleList<int>({1, 2}) ) ) {
-      std::cout << "-- LengthGreaterThanCeroAtConstructionWithInitializerList \t FAIL" << '\n';
+      std::cout << "\t-- LengthGreaterThanCeroAtConstructionWithInitializerList \t FAIL" << '\n';
       return -1;
   }
 
   if ( not LengthEqualToOneAfterOnePushBack( CircularSingleList<int>() )) {
-      std::cout << "-- LengthEqualToOneAfterOnePushBack \t FAIL" << '\n';
+      std::cout << "\t-- LengthEqualToOneAfterOnePushBack \t FAIL" << '\n';
       return -1;
   }
 
   if ( not LengthBackToCeroAfterOnePushAndOnePop( CircularSingleList<int>() )) {
-      std::cout << "-- LengthBackToCeroAfterOnePushAndOnePop \t FAIL" << '\n';
+      std::cout << "\t-- LengthBackToCeroAfterOnePushAndOnePop \t FAIL" << '\n';
       return -1;
   }
 
   if ( not LengthAfterThreePush( CircularSingleList<int>() )) {
-      std::cout << "-- LengthAfterThreePush \t FAIL" << '\n';
+      std::cout << "\t-- LengthAfterThreePush \t FAIL" << '\n';
       return -1;
   }
 
   if ( not ErrorAfterOnePushAndOnePop( CircularSingleList<int>() )) {
-      std::cout << "-- ErrorAfterOnePushAndOnePop \t FAIL" << '\n';
+      std::cout << "\t-- ErrorAfterOnePushAndOnePop \t FAIL" << '\n';
       return -1;
   }
 
-  std::cout << "\t ALL TEST SUCCEDED "<< '\n';
+  std::cout << '\n'
+            << "\t ----------------------" << '\n'
+            << "\t    ALL TEST SUCCEDED  " << '\n'
+            << "\t ----------------------" << '\n';
   return 0;
 }

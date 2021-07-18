a13d68c562a83fa44d5a857ab96351b52c6c9ab0
Matias M. Marceca <matias.marceca09@gmail.com>
2021-07-18 09:42:42 -0300

    move test to another file

--- structures/lists/single/list.h
+++ structures/lists/single/list.h
@@ -111,50 +111,50 @@ private:
 
 #endif // SINGLE_LIST_H
 
-// Unit Test
-template <typename T>
-bool LengthCeroAtEmptyConstruction(T && c)
-{
-  return c.length() == 0;
-}
-
-template <typename T>
-bool LengthGreaterThanCeroAtConstructionWithInitializerList(T&& c)
-{
-  return c.length() > 0;
-}
-
-template <typename T>
-bool LengthEqualToOneAfterOnePushBack(T&& c)
-{
-  c.Insert(1);
-  return c.length() == 1;
-}
-
-template <typename T>
-bool LengthBackToCeroAfterOnePushAndOnePop(T&& c)
-{
-  c.Insert(1);
-  c.Erase();
-  return c.length() == 0;
-}
-
-template <typename T>
-bool LengthAfterThreePush(T&& c)
-{
-  c.Insert(1);
-  c.Insert(2);
-  c.Insert(3);
-
-  return c.length() == 3;
-}
-
-template <typename T>
-bool ErrorAfterOnePushAndOnePop(T&& c)
-{
-  c.Insert(1);
-  c.Erase();
-  c.Erase();
-
-  return c.length() == false;
-}
+// // Unit Test
+// template <typename T>
+// bool LengthCeroAtEmptyConstruction(T && c)
+// {
+//   return c.length() == 0;
+// }
+//
+// template <typename T>
+// bool LengthGreaterThanCeroAtConstructionWithInitializerList(T&& c)
+// {
+//   return c.length() > 0;
+// }
+//
+// template <typename T>
+// bool LengthEqualToOneAfterOnePushBack(T&& c)
+// {
+//   c.Insert(1);
+//   return c.length() == 1;
+// }
+//
+// template <typename T>
+// bool LengthBackToCeroAfterOnePushAndOnePop(T&& c)
+// {
+//   c.Insert(1);
+//   c.Erase();
+//   return c.length() == 0;
+// }
+//
+// template <typename T>
+// bool LengthAfterThreePush(T&& c)
+// {
+//   c.Insert(1);
+//   c.Insert(2);
+//   c.Insert(3);
+//
+//   return c.length() == 3;
+// }
+//
+// template <typename T>
+// bool ErrorAfterOnePushAndOnePop(T&& c)
+// {
+//   c.Insert(1);
+//   c.Erase();
+//   c.Erase();
+//
+//   return c.length() == false;
+// }

71ac0555e7584bd1bc53a13812c4861ff82135ba
Matias M. Marceca <matias.marceca09@gmail.com>
2021-07-22 20:46:35 -0300

    struggling with cormen implementation of maxheapify

--- structures/priority_queue/main.cpp
+++ structures/priority_queue/main.cpp
@@ -3,8 +3,18 @@
 
 int main()
 {
-  PriorityQueue pq{8,2,0,4,1,3};
-  pq.PrintElements();
+  // PriorityQueue pq{7,2,9,4,1,5,3};
+  PriorityQueue pq{16,4,10,14,7,9,3,2,8,1};
+  // pq.BuildMaxHeap();
+  pq.MaxHeapify(1);
+
+  // auto const els = pq.getnum();
+
+  // std::cout << '\n';
+  // std::cout << '\n';
+  // std::size_t len = els.length();
+  // for (std::size_t i = 0; i < len; ++i)
+  //   std::cout << els[i] << ' ';
 
   std::cout << '\n';
   return 0;
--- structures/priority_queue/priority_queue.h
+++ structures/priority_queue/priority_queue.h
@@ -1,32 +1,73 @@
-#ifndef  PRIORITY_QUEUE_H
-#define  PRIORITY_QUEUE_H
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
 
-#include "vector.h"
+#include "../vector/vector.h"
 
 #include <iostream>
 #include <initializer_list>
+
+#include <math.h>
 
 class PriorityQueue
 {
 public:
   explicit PriorityQueue() = default;
   explicit PriorityQueue(std::initializer_list<int> elements) 
+    : ns_(elements)
   {
-    for (auto const & e : elements) 
-      numbers_.PushBack(e);
+    std::size_t heap_size = ns_.length();
+    for (std::size_t i = 0; i < heap_size; ++i)
+      std::cout << ns_[i] << ' ';
+
+    std::cout << '\n' << '\n';
   }
 
- ~PriorityQueue() {}
+  ~PriorityQueue() {}
 
- void PrintElements() const 
- {
-   std::size_t len = numbers_.length();
-   for (std::size_t i = 0; i < len; ++i)
-     std::cout << numbers_[i] << ' ';
- }
+  void BuildMaxHeap()
+  {
+    std::size_t len = ns_.length() / 2;
+    for (std::size_t i = 0; i < len; ++i)
+      MaxHeapify(i);
+  }
+
+  void MaxHeapify(int const i, int j = 0)
+  {
+    if ( j == 20 ) return;
+
+    std::size_t heap_size = ns_.length();
+    for (std::size_t i = 0; i < heap_size; ++i)
+      std::cout << ns_[i] << ' ';
+
+    std::cout << '\n';
+
+    int largest = 0;
+
+    int l = Left(i);
+    int r = Right(i);
+
+    if (l <= heap_size && ns_[l] > ns_[i])
+      largest = l;
+    else
+      largest = i;
+
+    if (r <= heap_size && ns_[r] > ns_[largest])
+        largest = r;
+
+    if (largest != i) {
+      ns_.swap(i, largest);
+      MaxHeapify(largest, ++j);
+    }
+  }
+
+  Vector<int> getnum() const { return ns_; }
 
 private:
- Vector<int> numbers_;
+  int Parent(int const i) const { return std::floor(i / 2); }
+  int Left(int const i) const { return i * 2; }
+  int Right(int const i) const { return (i * 2) + 1; }
+
+  Vector<int> ns_;
 };
 
 #endif // PRIORITY_QUEUE_H

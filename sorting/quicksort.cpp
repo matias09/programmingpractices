ad8666807885aed6b1d8aa6bf86dd23ec6336054
Matias M. Marceca <matias.marceca09@gmail.com>
2021-07-24 12:00:40 -0300

    false alarm, quicksort is not working, REVISE

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -1,21 +1,31 @@
-#include "../structures/vector/vector.h"
-
 #include <iostream>
+#include <vector>
 
-std::size_t Partition(Vector<int> & v, int const p, int const r)
+template <typename T>
+void swap(std::vector<T> & v, int i, int j)
+{
+  T tmp = v[i];
+  v[i] = v[j];
+  v[j] = tmp;
+}
+
+
+template <typename T>
+std::size_t Partition(std::vector<T> & v, int const p, int const r)
 {
   int x = v[r - 1];
   int i = p - 1;
 
   for(std::size_t j = p; j < r; ++j)
     if (v[j] <= x)
-      v.swap(j, ++i); 
+      swap(v, j, ++i); 
 
-  v.swap(x, ++i); 
+  swap(v, x, ++i); 
   return i;
 }
 
-void QuickSort(Vector<int> & v, int const p, int const r)
+template <typename T>
+void QuickSort(std::vector<T> & v, int const p, int const r)
 {
   if (p < r) {
     int q = Partition(v, p, r);
@@ -26,8 +36,9 @@ void QuickSort(Vector<int> & v, int const p, int const r)
 
 int main(int argc, const char* args[])
 {
-  Vector<int> v {2,8,7,1,3,5,6,4};
-  std::size_t len = v.length(); 
+  // TODO: Revise [2,8,70,1,3,5,6,4]
+  // Check with biggest numbers in different places.
+  std::vector<int> v {2,8,7,1,3,5,6,4,9};
 
   QuickSort(v, 0, len);
   for (std::size_t i = 0; i < len; ++i)
